ef5f464e750c4c0f01b5ab49453755a927a7934d
KhanyileBN <1421169@students.wits.ac.za>
2019-08-05 17:24:55 +0200

    kNN Graph

--- KNNGraph.java
+++ KNNGraph.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class KNNGraph {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        // accept inputs k, p, n and d
         String firstLine = scan.nextLine();
         int k, p, n, d, startX, startY, endX, endY;
         k = Integer.parseInt(firstLine.split(" ")[0]);
@@ -12,14 +13,17 @@ public class KNNGraph {
         n = Integer.parseInt(firstLine.split(" ")[2]);
         d = Integer.parseInt(firstLine.split(" ")[3]);
 
+        // accept starting point
         String startPoint = scan.nextLine();
         startX = Integer.parseInt(startPoint.split(",")[0]);
         startY = Integer.parseInt(startPoint.split(",")[1]);
 
+        // accept end point
         String endPoint = scan.nextLine();
         endX = Integer.parseInt(endPoint.split(",")[0]);
         endY = Integer.parseInt(endPoint.split(",")[1]);
 
+        // accept obstacles
         ArrayList<Integer> xObstacle = new ArrayList<>(2*p);
         ArrayList<Integer> yObstacle = new ArrayList<>(2*p);
         for (int i = 0; i < p; i++) {
@@ -31,15 +35,48 @@ public class KNNGraph {
             }
         }
 
+        // generate random points in a range
+        ArrayList<Integer> xCoord = randomPoints(n, d);
+        ArrayList<Integer> yCoord = randomPoints(n, d);
 
+        System.out.println(xCoord);
+        System.out.println(yCoord);
 
+        // remove points coinciding with obstacles
+        int t = n;
+        for(int g = 0; g < t; g++) {
+            for(int f = 0; f < 2*p-1; f++){
+                if(g>=0) {
+                    if (xCoord.get(g) >= xObstacle.get(f) && xCoord.get(g) <= xObstacle.get(f + 1)) {
+                        if (yCoord.get(g) >= yObstacle.get(f) && yCoord.get(g) <= yObstacle.get(f + 1)) {
+                            xCoord.remove(g);
+                            yCoord.remove(g);
+                            t--;
+                            g--;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(xObstacle);
+        System.out.println(yObstacle);
+        System.out.println(xCoord);
+        System.out.println(yCoord);
     }
 
-    public static ArrayList randomPoints(int n, int range){
+    private static ArrayList<Integer> randomPoints(int n, int range){
         ArrayList<Integer> Points = new ArrayList<>(n);
         Random rand = new Random();
-        int value = rand.nextInt(range);
+        for(int j = 0; j < n; j++) {
+            int myPoint = rand.nextInt(range);
+            Points.add(myPoint);
+        }
 
         return Points;
+    }
+
+    private static boolean KNN(int k) {
+        return false;
     }
 }
